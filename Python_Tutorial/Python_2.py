32b1c798cd462be0c72f8e7a2f842901a9d8d361
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-03-09 17:59:39 +0530

    dataTypes

---
+++ Python_Tutorial/Python_2.py
@@ -0,0 +1,26 @@
+a = 9
+b = "Lakshay"
+c = True
+d = complex(4,2)
+print(d)
+
+# This will throw an error as we can't concatinate two different types of datatypes
+# print(a+b)
+
+print("Type of a is ",type(a))
+print("Type of b is ",type(b))
+print("Type of c is ",type(c))
+print("Type of d is ",type(d))
+
+# Types of datatypes
+"""
+1. int  - Number
+2. float - Decimal Number
+3. complex - Complex Number
+4. str - String
+5. boolean - True or False
+6. list - List datatype used to store list(Mutable)
+7. tuple - Similar to list (Immutable)
+8. dict - Dictonary is used to map things("Name":"Lakshay")
+
+"""

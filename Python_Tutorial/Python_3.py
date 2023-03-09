b65263feb82a6a225108979902c472b01b82d7bf
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-03-09 19:58:32 +0530

    typeCasting

---
+++ Python_Tutorial/Python_3.py
@@ -0,0 +1,14 @@
+# typeCasting
+a = "9"
+b = "65"
+print(a+b)
+
+# This is how you can typeCast in Python
+# int(a) --> typeCast to int
+# This is a explicit typeCasting
+print(int(a)+int(b))
+
+# This is a implicit typeCasting
+c = 8.9
+d = 9
+print(c+d)

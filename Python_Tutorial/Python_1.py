6f58aee004656c45d7f7c210cd086250ef987708
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-03-09 13:09:30 +0530

    Printing Statements in Python

---
+++ Python_Tutorial/Python_1.py
@@ -0,0 +1,15 @@
+print("Hello")
+print("Name lakshay")
+# This is how you can write comments in Python
+
+print("Namaskar \n World")
+# This is an escape sequence character to print a new line
+
+print("Hello, This side \"Lakshay\"")
+# \" This is an escape sequence character
+
+# This will separate the three with ~ sign
+print("Hey",3,7,sep="~",end="123")
+print("Hey",3,7,sep="~",end="")
+print("Lakshay")
+# This will start the new line using the end the of new line as the origin Hey~3~7123Lakshay

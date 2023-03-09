951c2da535f9c6953f4fb70f7979289d31df2eb3
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-03-09 23:01:38 +0530

    Taking Input and Strings

---
+++ Python_Tutorial/Python_4.py
@@ -0,0 +1,19 @@
+# Taking Input
+a = input("Enter the number: ")
+b = input("Enter the second number: ")
+print(a+b)
+print(int(a)+int(b))
+
+# Strings
+c = "Lakshay"
+d = "Rahul"
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
+print(c[4]) # This will print the 4 index of the string c
+# print(c[10]) #This will throw an error
+
+# Using a for loop to print an entire array
+for character in d:
+    print(character)
